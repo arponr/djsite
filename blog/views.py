fae5a3bb4dcb7f4f190385745575c178310b8cff
Arpon Raksit <arpon.raksit@gmail.com>
2013-03-02 13:01:35 -0500

    starting full post view

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,9 @@ from blog.models import Post
 
 def blog(request):
     posts = Post.objects.all()
-    posts_left = [p for p in posts if p.id % 2 == 1]
-    posts_right = [p for p in posts if p.id % 2 == 0]
-    return render_to_response('blog.html', {'posts_left': posts_left,
-                                            'posts_right': posts_right})
+    left = [p for p in posts if p.id % 2 == 1]
+    right = [p for p in posts if p.id % 2 == 0]
+    return render_to_response('blog.html', {'left': left,
+                                            'right': right})
+
+
