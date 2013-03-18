b10e806b503313f0acf489b475484077776d71d4
Arpon Raksit <arpon.raksit@gmail.com>
2013-03-18 18:54:50 -0400

    create post: end-of-the-world

--- create.sh
+++ create.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 python convert.py $1
+git commit -a -m "create post: $1"
+git push heroku master
 heroku run python manage.py upload
 rm upload-temp
+git commit -a -m "delete upload-temp"
+git push heroku master
