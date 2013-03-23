f82d675e367dfcb3b34a8682834c8e92e28df647
Arpon Raksit <arpon.raksit@gmail.com>
2013-03-22 23:09:40 -0400

    home

--- djsite/views.py
+++ djsite/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render_to_response
 
-def about(request):
-    return render_to_response('about.html', {})
-
-def resume(request):
-    return render_to_response('resume.html', {})
+def home(request):
+    return render_to_response('home.html', {})
 
 def work(request):
     return render_to_response('work.html', {})
