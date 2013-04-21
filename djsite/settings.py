b2bcd3fabe941e5783bd566901211e2e71ab06b3
Arpon Raksit <arpon.raksit@gmail.com>
2013-04-20 22:12:24 -0400

    media root

--- djsite/settings.py
+++ djsite/settings.py
@@ -59,12 +59,12 @@ STATICFILES_DIRS = (
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media')
+MEDIA_ROOT = STATIC_ROOT
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/static/media/'
+MEDIA_URL = STATIC_URL
 
 # List of finder classes that know how to find static files in
 # various locations.
