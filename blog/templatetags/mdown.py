316f2cc1798df550841e70bae4d0fa3450cf2e78
Arpon Raksit <arpon.raksit@gmail.com>
2013-04-21 20:24:03 -0400

    markdown mathjax

--- blog/templatetags/mdown.py
+++ blog/templatetags/mdown.py
@@ -1,10 +1,23 @@
 from django import template
-import markdown, blog.mdx_mathjax
+import markdown
 
 register = template.Library()
 
+class MathJaxPattern(markdown.inlinepatterns.Pattern):
+    def __init__(self):
+        markdown.inlinepatterns.Pattern.__init__(self, r'(?<!\\)(\$\$?)(.+?)\2')
+
+    def handleMatch(self, m):
+        node = markdown.util.etree.Element('mathjax')
+        node.text = markdown.util.AtomicString(m.group(2) + m.group(3) + m.group(2))
+        return node
+
+class MathJaxExtension(markdown.Extension):
+    def extendMarkdown(self, md, md_globals):
+        md.inlinePatterns.add('mathjax', MathJaxPattern(), '<escape')
+
 def mdown(value):
-    md = markdown.Markdown(extensions=['mathjax', 'smartypants'])
+    md = markdown.Markdown(extensions=[MathJaxExtension()])
     return md.convert(value)
 
 register.filter('mdown', mdown)
