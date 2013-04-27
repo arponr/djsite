5e594d8389d954a660fd2c4a18b2355d21edade5
Arpon Raksit <arpon.raksit@gmail.com>
2013-04-27 16:51:01 -0400

    preview toggle

--- djsite/static/j.js
+++ djsite/static/j.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     $('.hidden').hide();
     $('.toggle').click(function(e) {
 	e.preventDefault();
-	$(this).next('.hidden').slideToggle(300);
+	$(this).parent().next('.hidden').slideToggle(300);
     });
 });
