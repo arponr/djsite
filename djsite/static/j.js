04fe21ac2e6797a345a78a976390abff7e020147
Arpon Raksit <arpon.raksit@gmail.com>
2013-04-28 00:16:23 -0400

    white

--- djsite/static/j.js
+++ djsite/static/j.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-    $('.prev').hide();
     $('.toggle').click(function(e) {
 	e.preventDefault();
-	$(this).parent().parent().next('.prev').slideToggle(300);
+	var i = $('.post').index($(this).parent('.post'));
+	$('.post').each(function(j, obj) {
+	    if (j < i) {
+		obj.removeClass().addClass('post '+'colour'+j);
+	    }
+	    else if (j > i) {
+		obj.removeClass().addClass('post '+'colour'+(j-1));
+	    }
+	    else {
+		obj.removeClass().addClass('post');
+	    }
+	});
+	$(this).parent('.actions').next('.prev').slideToggle(300);
     });
+    $('.prev').hide();
+    $('.toggle:first').trigger();
 });
