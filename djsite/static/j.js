2dc46a5f2d4d2d358420391ba0330802d3998b98
Arpon Raksit <arpon.raksit@gmail.com>
2013-04-28 19:04:41 -0400

    no bars, list

--- djsite/static/j.js
+++ djsite/static/j.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
-    $('.toggle.down').click(function(e) {
+    $('.toggle.up, .toggle.down').click(function(e) {
 	e.preventDefault();
-	$(this).parent().next('.hidden').slideDown();
-	$(this).removeClass('down').addClass('up');
-    });
-    $('.toggle.up').click(function(e) {
-	e.preventDefault();
-	$(this).parent().next('.hidden').slideUp();
-	$(this).removeClass('up').addClass('down');
+	$(this).parent().next('.hidden').slideToggle();
+	$(this).toggleClass('down up');
     });
 });
