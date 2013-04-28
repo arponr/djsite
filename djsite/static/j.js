723c281e96609310da5f3069710b73bcf8eaca91
Arpon Raksit <arpon.raksit@gmail.com>
2013-04-28 16:33:53 -0400

    no bars, list

--- djsite/static/j.js
+++ djsite/static/j.js
@@ -1,27 +1,9 @@
 $(document).ready(function() {
-    $('.toggle').click(function(e) {
+    $('.post li:before').click(function(e) {
 	e.preventDefault();
-	/*
-	var i = post.index($(this).closest('.post'));
-	post.each(function(j, obj) {
-	    if (j < i) {
-		$(this).parent().removeClass().addClass('colour colour' + (j+1));
-	    }
-	    else if (j > i) {
-		$(this).parent().removeClass().addClass('colour colour' + j);
-	    }
-	    else {
-		$(this).parent().removeClass();
-	    }
-	});
-	if (i != last) {	    
-	    prev.slideUp(400);
-	    last = i;
-	}
-	*/
-	$(this).next('.hidden').slideToggle();
+	$(this).find('.hidden').slideToggle();
     });
 
     $('.hidden').hide();    
-    $('.toggle').eq(0).trigger('click');
+    $('.post li:before').eq(0).trigger('click');
 });
