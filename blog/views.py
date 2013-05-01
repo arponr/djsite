e2036d84389b5ee60753e3b884415ed299df384e
Arpon Raksit <arpon.raksit@gmail.com>
2013-04-30 21:45:57 -0400

    no bars, list

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from blog.models import Post, Category
 def blog(request):
     return render_to_response('blog.html', 
                               {'posts': list(Post.objects.all()) * 4,
-                               'cats': Category.objects.all()}) 
+                               'cats': list(Category.objects.all()) * 4}) 
 
 def category(request, sl):
     c = get_object_or_404(Category, slug=sl)
@@ -16,6 +16,6 @@ def category(request, sl):
 
 def post(request, sl):
     p = get_object_or_404(Post, slug=sl)
-    return render_to_response('post.html', {'p': p})
+    return render_to_response('post.html', {'p': p, 'cats': Category.objects.all()})
 
 
